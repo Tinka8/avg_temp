dc10bf66966c2093459db4846b5a77b0dd1487b8
Tinka8 <lenmatka818@gmail.com>
2021-10-01 13:39:44 +0200

    space

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ t1 = float(input("zadajte teplotu rano: "))
 t2 = float(input("na obed: "))
 t3 = float(input("vecer: "))
 t = (t1 + t2 + t3 + t3)/4
+
 print("priemerna denna teplota je", round(t, 2), "stupnov celzia")
